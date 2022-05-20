af5f735d1e535ee3545a29fbb933af73bb8a5953
Julián Egea <julian.egea.ponce@gmail.com>
2022-05-20 21:11:37 +0200

    Revert "Dev to main for preview release"

--- actors/actor.ts
+++
@@ -1,8 +0,0 @@
-export interface Actor {
-    position: {x:number, y:number}
-    ctx: CanvasRenderingContext2D;
-
-    update(delta: number, canvasWidth:number, canvasHeight:number);
-    draw(delta:number, ctx:CanvasRenderingContext2D);
-    keyboard_event(key:string);
-}
--- assets/utils/Constants.ts
+++
@@ -1,6 +0,0 @@
-export const Constants = {
-    KEY_EVENT_KEYDOWN: "keydown",
-    KEYDOWN_ARROW_RIGHT: "ArrowRight",
-    KEYDOWN_ARROW_LEFT: "ArrowLeft",
-    KEYDOWN_SPACEBAR: " "
-}
